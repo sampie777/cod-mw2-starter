51478ad8461cbe8cddd32f9ec2cdd5004cedf125
S. Jansen <sa.jansen1@gmail.com>
2022-01-14 11:38:59 +0200

    test: Removed broadcasting test as maven fails to ignore it.

--- src/test/kotlin/nl/sajansen/codmw2starter/ipScanner/udpSniffer/NetworkSnifferTest.kt
+++ src/test/kotlin/nl/sajansen/codmw2starter/ipScanner/udpSniffer/NetworkSnifferTest.kt
@@ -1,12 +1,11 @@
 package nl.sajansen.codmw2starter.ipScanner.udpSniffer
 
-import org.junit.Test
-
 class NetworkSnifferTest {
-    @Test
-    fun test() {
-        NetworkSniffer.start()
-
-        while(NetworkSniffer.others.size == 0) {}
-    }
+//    @Ignore
+//    @Test
+//    fun test() {
+//        NetworkSniffer.start()
+//
+//        while(NetworkSniffer.others.size == 0) {}
+//    }
 }
